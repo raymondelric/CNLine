a9a14c142b81396f32524f83e6b721ed7928dc8b
b03902045 <b03902045@ntu.edu.tw>
2017-01-15 02:50:42 +0800

    remove redundant file

--- src/server/download_file.js
+++ src/server/download_file.js
@@ -1,25 +1,40 @@
 var fs = require('fs');
 var net = require('net');
+var sleep = require('sleep');
 
-var HOST = '127.0.0.1';
+var HOST = '140.112.30.44';
 var PORT = 9001;
+    
+console.log("[File Server Start]");
 
 net.createServer( function(sock) {
     
-    console.log("[File Server Start]");
+    console.log("[Connected File Server]");
 
     sock.on('data',function (data){
         
         msg = data.toString();
-        var input = msg.split(/[\/,\n]+/);
+        //var input = msg.split(/[\/,\n]+/);
         //var data = fs.readFileSync("./file/"+input[0]);
-        console.log("[Data] " + data);
+        console.log("[FS Data] " + data);
         fs.writeFileSync("./file/client_request",data);
 
         console.log("[Pass the req to Slave]");
         var child = require('child_process').fork(__dirname + '/client_request'); 
         child.send('socket',sock);
 
+        sleep.sleep(1000);
+
+    });
+
+    sock.on('close',function(data){
+            console.log("[Remove]");
     });
 
 }).listen(PORT,HOST);
+
+
+process.on('uncaughtException',function(err){
+    console.log(err.throw);
+    console.log("[Abort]");
+});
